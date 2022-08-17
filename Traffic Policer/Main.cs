e85f58286a7a94dc524b3f7e55d3cd43d9a55296
Vari <72755263+Variapolis@users.noreply.github.com>
2022-08-17 20:48:39 +0100

    build(traffic-policer): update dependency versions

--- Traffic Policer/Main.cs
+++ Traffic Policer/Main.cs
@@ -63,12 +63,12 @@ namespace Traffic_Policer
 
         }
         //Dependencies
-        internal static Version Albo1125CommonVer = new Version("6.6.3.0");
-        internal static Version MadeForGTAVersion = new Version("1.0.1604.1");
-        internal static float MinimumRPHVersion = 0.51f;
+        internal static Version Albo1125CommonVer = new Version("6.6.5.0");
+        internal static Version MadeForGTAVersion = new Version("1.0.2699.0");
+        internal static float MinimumRPHVersion = 1.87f;
         internal static string[] AudioFilesToCheckFor = new string[] { "lspdfr/audio/scanner/Traffic Policer Audio/OTHER_UNIT_TAKING_CALL/OTHER_UNIT_TAKING_CALL_01.wav", "lspdfr/audio/scanner/Traffic Policer Audio/Crimes/CRIME_DUI_01.wav" };
-        internal static Version RAGENativeUIVersion = new Version("1.6.3.0");
-        internal static Version MadeForLSPDFRVersion = new Version("0.4.2");
+        internal static Version RAGENativeUIVersion = new Version("1.9");
+        internal static Version MadeForLSPDFRVersion = new Version("0.4.9");
 
         internal static string FileID = "8303";
         internal static string DownloadURL = "https://www.lcpdfr.com/files/file/8303-traffic-policer-breathalyzer-traffic-offences-speed-detection-more/";
